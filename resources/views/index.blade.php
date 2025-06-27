2c79ae95b6e3d4cefd05c0a100a692c9fcc693f3
reymartin <reymartinagluya8@gmail.com>
2025-06-27 11:45:00 +0800

    error 404

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,26 +2,34 @@
 <html>
 <head>
     <title>All About COC - Player Search</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
-    <h1>Clash of Clans Player Search</h1>
 
-    <form action="{{ route('search') }}" method="POST">
-        @csrf
-        <input type="text" name="tag" placeholder="Enter player tag (e.g. 88JY8P2)" required>
-        <button type="submit">Search</button>
-    </form>
+    <div class="min-h-screen flex flex-col justify-center items-center bg-blue-200">
 
-    @isset($player)
-        <h2>Player Info</h2>
-        <p><strong>Name:</strong> {{ $player['name'] }}</p>
-        <p><strong>Town Hall:</strong> {{ $player['townHallLevel'] }}</p>
-        <p><strong>Trophies:</strong> {{ $player['trophies'] }}</p>
-        <p><strong>Clan:</strong> {{ $player['clan']['name'] ?? 'No Clan' }}</p>
-    @endisset
+        <div class="bg-white/35 backdrop-blur-2xl p-15 border-none rounded-xl">
+            <h1 class="mb-7 text-2xl font-bold">Clash of Clans Player Search</h1>
 
-    @isset($error)
-        <p style="color:red;">{{ $error }}</p>
-    @endisset
+            <form class="flex flex-col items-center" action="{{ route('search') }}" method="POST">
+                @csrf
+                <input class="mb-3 border border-gray-400 p-2 rounded-sm bg-white/35 focus:backdrop-blur-2xl focus:border-blue-400 focus:border-2 outline-none"  type="text" name="tag" placeholder="88JY8P2" required>
+                <button class="w-auto px-7 border-none rounded-sm py-2 bg-blue-400 hover:bg-blue-600 text-white hover:shadow-2xl" type="submit">Search</button>
+            </form>
+        </div>   
+
+        @isset($player)
+            <h2>Player Info</h2>
+            <p><strong>Name:</strong> {{ $player['name'] }}</p>
+            <p><strong>Town Hall:</strong> {{ $player['townHallLevel'] }}</p>
+            <p><strong>Trophies:</strong> {{ $player['trophies'] }}</p>
+            <p><strong>Clan:</strong> {{ $player['clan']['name'] ?? 'No Clan' }}</p>
+        @endisset
+
+        @isset($error)
+            <p style="color:red;">{{ $error }}</p>
+        @endisset
+
+    </div>
 </body>
 </html>
