13897aac9d8bfe0e1a843e8f442fd21987561f7e
ReyVenge <123310086+ReyMartin19@users.noreply.github.com>
2025-07-06 13:03:59 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,7 @@ php artisan key:generate --force
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
+# Add this to generate the sessions table
+php artisan session:table
+
 php artisan migrate --force
